52cef1dcde38d5327fcfb9b95565e16190bf39cd
Noam <noamatzil@Noams-MacBook-Pro.local>
2024-02-20 18:31:50 +0200

    searchBar

--- client/js/client.js
+++ client/js/client.js
@@ -246,3 +246,22 @@ async function initAll() {
   document.querySelector(".table").innerHTML = strOrders.join("");
   //   console.log(orders);
 }
+
+async function searchProducts() {
+  try {
+    const data = await makeFetchRequest("/api/filterBySearchBar");
+    const products = data.allFilteredProducts;
+    const searchInput = document.getElementById("searchInput");
+    const query = searchInput.value.toLowerCase().trim();
+
+    const productsList = document.querySelector(".products-list");
+    productsList.innerHTML = "";
+
+    const filteredProducts = products.filter((product) => {
+      return product.productName.toLowerCase().includes(query);
+    });
+    renderProducts(filteredProducts);
+  } catch (error) {
+    console.error("Error searching products:", error);
+  }
+}
